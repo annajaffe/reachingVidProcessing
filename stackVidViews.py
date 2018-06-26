7ef3602b05b11e6bf352ce8375f971d1c992fe5f
Anna Jaffe <annawjaffe@gmail.com>
2018-06-26 17:25:36 -0400

    changed name of generated concatenated video

--- stackVidViews.py
+++ stackVidViews.py
@@ -1,9 +1,9 @@
 '''
 stackVidViews.py takes 2 input .avi videos and stacks them one on top of another.
 First, cd into the folder with the videos to be stacked.
-The user should pass in the side view first (top), the front view (bottom),
-and then the output filename.
-E.g., python stackVidViews.py sideCam.avi frontCam.avi combined.avi
+The user should pass in 2 video filenames (side and front), which will generate
+an output .avi with the same date as the input videos if the user does not
+specify an output filename.
 '''
 
 from skimage.io import imsave
@@ -14,8 +14,8 @@ from moviepy.editor import VideoFileClip, clips_array
 
 def main():
 	if len(sys.argv) > 1:
-		avi_side = sys.argv[1]
-		avi_front = sys.argv[2]
+		avi_top = sys.argv[1]
+		avi_bottom = sys.argv[2]
 	else:
 		raise RuntimeError('AVI file was not specified')
 	if len(sys.argv) == 4:
@@ -23,15 +23,15 @@ def main():
 		if not out_file.endswith('avi'):
 			out_file += '.avi'
 	else:
-		out_file = 'concatVid.avi'
+		out_file = 'concatVid_' + avi_top[-26:-16] + '.avi' # yyyy-mm-dd
 
-	# Load in side and front avi files:
-	clip_side = VideoFileClip(avi_side)
-	clip_front = VideoFileClip(avi_front)
-	w, h = clip_side.size
+	# Load in avi files:
+	clip_top = VideoFileClip(avi_top)
+	clip_bottom = VideoFileClip(avi_bottom)
+	w, h = clip_top.size
 
 	# Stack videos and save:
-	final_clip = clips_array([[clip_side], [clip_front]])
+	final_clip = clips_array([[clip_top], [clip_bottom]])
 	final_clip.write_videofile(out_file, codec='rawvideo') # write as .avi
 
 if __name__ == '__main__':
