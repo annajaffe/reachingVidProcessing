643df4a99c746c7f630dbdc258e51f5a7995b51a
Anna Jaffe <annawjaffe@gmail.com>
2018-06-26 16:46:00 -0400

    slight cleanup

--- h5toPng.py
+++ h5toPng.py
@@ -1,4 +1,3 @@
-# import pdb
 from skimage.io import imsave
 import os # added AJ
 import h5py
@@ -13,6 +12,8 @@ def main():
     new_fname = input_file[:-3] + '-image-{:08d}.png'
 
     # added AJ:
+    # Make a new folder in the LEAP folder that will save the resulting
+    # images as chosen from clustering
     baseFolder = r'C:\Users\SabatiniLab\Documents\GitHub\leap' # need to put r in front for "raw string"
     savePath = os.path.join(baseFolder, input_file[:-3])
     if not os.path.exists(savePath):
@@ -23,8 +24,6 @@ def main():
     for i, frame in enumerate(f['box']):
         new_fname_path = os.path.join(savePath, new_fname.format(i)) # save images into new folder
         imsave(new_fname_path, frame[0].T) # transpose images so they're in original recorded format
-
-# pdb.set_trace()
 
 if __name__ == '__main__':
     main()
